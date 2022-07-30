7bff949d95d2563d2472f7dfc2159309aecc8500
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-07-30 20:52:30 -0300

    Add files necessary to render the app

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { LoginForm } from "./Pages/Login/LoginForm"
+import "./styles/styles.css"
+
+export function App() {
+    return (
+        <div className="page">
+            <LoginForm />
+        </div>
+    )
+}
---
+++ client/src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { createRoot } from "react-dom/client"
+import { App } from "./App"
+
+
+const root = document.querySelector("div#root") as HTMLDivElement
+
+createRoot(root).render(<App/>)
