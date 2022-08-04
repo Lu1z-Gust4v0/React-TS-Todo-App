a3dee66ce761ea5fecfb642357e2cd54185a2c0d
Luiz Gustavo <gustavo1204.gc@gmail.com>
2022-08-03 22:16:53 -0300

    Create 'parseTimestamp' function

--- client/src/Pages/Home/TodoItem.tsx
+++ client/src/Pages/Home/TodoItem.tsx
@@ -5,11 +5,25 @@ import { TodoItemProps } from "../../types/customTypes"
 
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo, toggleTodo, removeTodo }) => {    
+    
+    const parseTimestamp = (timestamp: string): string => {
+        const date = new Date(timestamp)
+        const list = [date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes()]
+        // add a leading 0
+        const [ year, month, day, hour, minutes, ] = list.map(item => {
+            return String(item).padStart(2, "0")
+        })
+
+        return `${day}/${month}/${year} - ${hour}:${minutes}`
+    }
+    
     return (
         <div className={todo.status ? "todo-card" : "todo-card disabled"}>
             <div className="card-heading">
                 <h2>{todo.title}</h2>
-                <span className="timestamp">{todo.createdAt}</span>
+                <span className="timestamp">
+                    {todo.createdAt? parseTimestamp(todo.createdAt) : ""}
+                </span>
             </div>
             <div className="card-body">
                 <div className="card-row">
